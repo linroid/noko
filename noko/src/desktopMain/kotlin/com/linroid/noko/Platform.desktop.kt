bab93848021f61a9e7da62459293d3b6e8d0ad97
Lin Zhang <linroid@gmail.com>
2022-03-16 07:04:20 +0800

    Fix native library loading when running desktop example

--- noko/src/desktopMain/kotlin/com/linroid/noko/Platform.desktop.kt
+++ noko/src/desktopMain/kotlin/com/linroid/noko/Platform.desktop.kt
@@ -1,6 +1,9 @@
 package com.linroid.noko
 
 import okio.FileSystem
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
 actual object Platform {
 
@@ -17,10 +20,29 @@ actual object Platform {
     return isDebuggerConnected
   }
 
+  @Suppress("UnsafeDynamicallyLoadedCode")
   actual fun loadNativeLibraries() {
-    // val libDir = File.createTempFile("noko", "noko")
-    // libDir.mkdirs()
-    // println(libDir)
-    System.loadLibrary("noko")
+    try {
+      System.loadLibrary("noko")
+    } catch (error: UnsatisfiedLinkError) {
+      val libDir = Files.createTempDirectory("noko")
+      val libNoko = extractResourceTo(System.mapLibraryName("noko"), libDir)
+      val libNode = extractResourceTo(System.mapLibraryName("node.93"), libDir)
+      System.load(libNode.absolutePath)
+      System.load(libNoko.absolutePath)
+    }
+  }
+
+  private fun extractResourceTo(path: String, targetDir: Path): File {
+    val stream = Platform::class.java.classLoader.getResourceAsStream(path)
+    checkNotNull(stream) { "Couldn't found $path" }
+    stream.use { input ->
+      val file = File(targetDir.toFile(), File(path).name)
+      file.createNewFile()
+      file.outputStream().use { output ->
+        input.copyTo(output)
+      }
+      return file
+    }
   }
 }
